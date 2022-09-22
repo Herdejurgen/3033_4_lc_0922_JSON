c2bc046ea463b1c19afc82038bf9e283ba98ea73
Jack Herdejurgen <Jack.D.Herdejurgen@ou.edu>
2022-09-22 10:14:52 -0500

    Add project files.

---
+++ 3033_4_lc_0922_JSON/Program.cs
@@ -0,0 +1,52 @@
+﻿// Jack Herdejurgen 113436899
+// Sept 22, 2022
+// 
+
+using System.Security.Cryptography.X509Certificates;
+
+string testStr;
+testStr = "No electronic materials (You could NOT look at your previous code on your computer) Open notes, open book. You can bring printed codes.";
+
+int numChar = testStr.Count((x) => { if (x == 'a') { return true; } else { return false; } });
+int numChar2 = testStr.Count(
+    x => "abcd".Contains(x)
+    );
+
+Console.WriteLine(numChar2);
+
+List<double> gradeslist = new List<double>();
+
+gradeslist.Add(75);
+gradeslist.Add(-97);
+gradeslist.Add(86);
+gradeslist.Add(68);
+gradeslist.Add(87);
+gradeslist.Add(-99);
+
+double highest = 0;
+
+foreach(double x in gradeslist)
+{
+    if (x > highest)
+    {
+        highest = x;
+    }
+}
+double highest2 = gradeslist.Max(x => Math.Abs(x));
+
+Console.WriteLine(highest);
+Console.WriteLine(highest2);
+
+// min -> max
+gradeslist.Sort(
+        (x,y) =>
+        {
+            if (x > y) { return 1; }
+            else if( x==y) { return 0; }
+            else { return -1; }
+        }
+    );
+Console.WriteLine("After Sort");
+foreach (double x in gradeslist) { Console.Write($"{x},"); }
+
+Console.ReadLine();
---
+++ var/Program.cs
@@ -0,0 +1,41 @@
+﻿// var
+
+string nameStr = "Jack";
+
+string result = AddFunctionToString.FirstLast(nameStr);
+string result2 = nameStr.FirstLast();
+Console.WriteLine(result2);
+
+var nameStrCard2 = nameStr;
+
+var age = 20;
+
+var stu1 = new Student{ age=24,name=nameStr};
+var stu2 = new Student(23, "Kate");
+
+var p1 = new { name = "Aiden", weight = 154.3 };
+var anon = p1.name;
+
+Console.ReadLine();
+
+public class Student
+{
+    public string name { get; set; }
+    public int age { get; set; }
+    public Student()
+    {
+
+    }
+    public Student(int age, string name)
+    {        
+        this.age = age;
+        this.name = name;
+    }
+}
+public static class AddFunctionToString // Adding this below adds it to all strings
+{
+    public static string FirstLast(this string str)
+    {
+        return string.Format($"{str[0]}{str[str.Length-1]}");
+    }
+}
